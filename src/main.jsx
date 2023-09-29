34afe4da4242fe4dbba1af9c905234ca0d38107c
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-29 19:03:10 +0530

    login and signup ready

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './Home/Home';
+import Login from './Login/Login';
+import Register from './Register/Register';
+import Root from './Root/Root';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children:[
+        {
+          path:"/",
+          element:<Home></Home>,
+        },
+        {
+          path:"/login",
+          element:<Login></Login>,
+        },
+        {
+          path:"/register",
+          element:<Register></Register>,
+        }
+    ]
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
